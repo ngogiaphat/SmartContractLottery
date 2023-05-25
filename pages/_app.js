66aa8da3f90bbbf50156939e7602a78f66250670
ngogiaphat <ngogiaphat0802@gmail.com>
2023-05-26 00:51:23 +0700

    ...

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,12 @@ import { MoralisProvider } from "react-moralis";
 import { NotificationProvider } from "web3uikit";
 import "../styles/globals.css";
 function MyApp({Component, pageProps}){
-	return(
-		<MoralisProvider initializeOnMount = {false}>
-			<NotificationProvider>
-				<Component {...pageProps}/>
-			</NotificationProvider>
-		</MoralisProvider>
-	);
+  return(
+    <MoralisProvider initializeOnMount = {false}>
+      <NotificationProvider>
+        <Component {...pageProps}/>
+      </NotificationProvider>
+    </MoralisProvider>
+  );
 }
 export default MyApp;
